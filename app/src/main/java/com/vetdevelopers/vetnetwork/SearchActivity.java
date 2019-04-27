65b923950748681496a9bb7de25c2a9369cd9f2f
Touhid <md.touhidul.islam.96@gmail.com>
2019-04-28 04:42:54 +0600

    Minor fix! (4)

--- app/src/main/java/com/vetdevelopers/vetnetwork/SearchActivity.java
+++ app/src/main/java/com/vetdevelopers/vetnetwork/SearchActivity.java
@@ -1,11 +1,18 @@
 package com.vetdevelopers.vetnetwork;
 
-
-import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,18 +41,17 @@ import java.util.Map;
 import java.util.Set;
 
 public class SearchActivity extends AppCompatActivity
-{
+        implements NavigationView.OnNavigationItemSelectedListener {
+
     private EditText name, phone;
     private Spinner postingAreaSpinner, districtSpinner, divisionSpinner;
     private Button searchButton;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
 
         name = (EditText) findViewById(R.id.search_name);
         phone = (EditText) findViewById(R.id.search_phone);
@@ -205,6 +211,9 @@ public class SearchActivity extends AppCompatActivity
 
             }
         });
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     private void setPostingAreaSpinner()
@@ -327,5 +336,35 @@ public class SearchActivity extends AppCompatActivity
         };
 
         MySingleton.getInstance(SearchActivity.this).addToRequestQueue(stringRequest);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_reportActivity) {
+
+            Intent reportForBrowsersIntent = new Intent(SearchActivity.this, ReportForBrowsersActivity.class);
+            startActivity(reportForBrowsersIntent);
+
+        } else if (id == R.id.nav_aboutUs) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
